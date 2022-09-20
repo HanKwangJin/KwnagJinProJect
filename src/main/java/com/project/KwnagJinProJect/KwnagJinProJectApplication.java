6c0185d3a1562ec71741d8bc8be5528ae6414ee0
HanKwangJin <hkj556@naver.com>
2022-09-20 19:48:52 +0900

    Initial commit

---
+++ src/main/java/com/project/KwnagJinProJect/KwnagJinProJectApplication.java
@@ -0,0 +1,13 @@
+package com.project.KwnagJinProJect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwnagJinProJectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwnagJinProJectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/KwnagJinProJect/KwnagJinProJectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.KwnagJinProJect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwnagJinProJectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
