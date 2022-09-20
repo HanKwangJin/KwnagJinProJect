ec899af5ec36a722697262e5af9aaf214611bc02
HanKwangJin <hkj556@naver.com>
2022-09-21 00:01:48 +0900

    html

---
+++ src/main/java/com/project/KwnagJinProJect/kwangJinController/JinController.java
@@ -0,0 +1,21 @@
+package com.project.KwnagJinProJect.kwangJinController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JinController {
+
+    @GetMapping("/")
+    public String home(){
+        return "kwangJinHome";
+    }
+    @GetMapping("kwangJin/signup")
+    public String signUp(){
+         return "kwangJinSignUp";
+    }
+    @GetMapping("kwangJin/list")
+    public String list(){
+         return "kwangJinList";
+    }
+}
